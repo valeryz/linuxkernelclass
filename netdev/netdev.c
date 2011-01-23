4e72043b6e032ffcc13b139903e09d78fa8a8ea6
Valeriy Zamarayev <valeriy.zamarayev@gmail.com>
2011-01-24 01:41:22 +0200

    developing netdev

--- netdev/netdev.c
+++ netdev/netdev.c
@@ -20,11 +20,6 @@
 
 #define MY_MOD_NAME "netdev"
 
-/* Ok, I understand the value of gathering all module vars in a sturct, but
- * let's not complicate things for such as simple module. They are static so
- * pose no problem for the linker */
-static struct proc_dir_entry *proc_dir;
-static struct proc_dir_entry *proc_info;
 
 struct net_device *nd;
 
@@ -44,53 +39,21 @@ module_param(debug_level, uint, S_IRUGO|S_IWUSR);
 #define DBG(...)
 #endif
 
-static int read_proc(char *page, char **start, off_t off,
-		     int count, int *eof, void *data)
-{
-	int n;
-
-	/*  STUB */
-	n = 0;
-
-	*eof = 1;
-	return (n);
-}
-
-static int __init setup_procfs_entry(void)
-{
-	proc_dir = proc_mkdir(MY_MOD_NAME, NULL);
-	if (unlikely(!proc_dir)) {
-		printk(KERN_ERR "unable to create /proc/%s\n", MY_MOD_NAME);
-		return (-1);
-	}
-
-	proc_info = create_proc_read_entry(MY_MOD_NAME, S_IRUGO, proc_dir,
-					   read_proc, NULL);
-	if (unlikely(!proc_info)) {
-		printk(KERN_ERR "cannot create proc entry\n");
-		remove_proc_entry(MY_MOD_NAME, NULL);
-		return (-1);
-	}
-
-	proc_info->read_proc = read_proc;
-	proc_info->data = NULL;
-
-	return (0);
-}
-
 int netdev_open(struct net_device *dev)
 {
 	/* set mac address */
 	static const char mymacaddr[ETH_ALEN] = "AABBCC";
 
-	memcpy(dev->dev_addr, mymacaddr, ETH_ALEN);
+	/* memcpy(dev->dev_addr, mymacaddr, ETH_ALEN); */
+
+	netif_start_queue(dev);
 
 	return 0;
 }
 
 int netdev_stop(struct net_device *dev)
 {
-	/* TODO implement */
+	netif_stop_queue(dev);
 	return 0;
 }
 
@@ -132,9 +95,6 @@ static int __init create_net_device(void)
 
 static int __init netdev_init(void)
 {
-	if (unlikely(setup_procfs_entry() == -1))
-		return -1;
-
 	if (unlikely(create_net_device()))
 		return -1;
 
@@ -144,6 +104,9 @@ static int __init netdev_init(void)
 
 static void __exit netdev_exit(void)
 {
+	unregister_netdev(nd);
+	free_netdev(nd);
+
 	DBG(2, KERN_INFO, "netdev unloaded\n");
 }
 
